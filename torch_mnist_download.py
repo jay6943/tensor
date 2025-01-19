82528691a04f1e48517e681bdbb608982e5d0363
Administrator <uetri@naver.com>
2025-01-19 22:11:32 +0900

    Samsung notebook

--- torch_mnist_download.py
+++ torch_mnist_download.py
@@ -1,14 +1,35 @@
 import torchvision.datasets as tds
 import torchvision.transforms as ttr
 
-tds.FashionMNIST(
-  root='../data/torch',
-  train=True,
-  download=True,
-  transform=ttr.ToTensor())
 
-tds.FashionMNIST(
-  root='../data/torch',
-  train=False,
-  download=True,
-  transform=ttr.ToTensor())
+def number_MIST():
+  tds.MNIST(
+    root='../data/torch',
+    train=True,
+    download=True,
+    transform=ttr.ToTensor()
+  )
+  tds.MNIST(
+    root='../data/torch',
+    train=False,
+    download=True,
+    transform=ttr.ToTensor()
+  )
+
+
+def fashion_MNIST():
+  tds.FashionMNIST(
+    root='../data/torch',
+    train=True,
+    download=True,
+    transform=ttr.ToTensor()
+  )
+  tds.FashionMNIST(
+    root='../data/torch',
+    train=False,
+    download=True,
+    transform=ttr.ToTensor()
+  )
+
+
+if __name__ == '__main__': number_MIST()
